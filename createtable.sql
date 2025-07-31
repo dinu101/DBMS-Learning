ef5044faf753d12e7bc385b595d26e7269d7e249
dinu101 <dr975208@gmail.com>
2025-07-31 12:56:28 -0700

    initial commit

---
+++ createtable.sql
@@ -0,0 +1,12 @@
+--      __DATABASE___
+-- This Is For Creating Database
+CREATE DATABSE School;
+
+--      TABLE CREATION
+-- This is for creating a table 
+CREATE TABLE Students(
+ID INT PRIMARY KEY,
+Name VARCHAR(50);
+Age INT,
+City VARCHAR(50)
+);
