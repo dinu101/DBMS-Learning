95525770b4097f3c294b8add918db55caeb2c474
dinu101 <dr975208@gmail.com>
2025-08-01 05:51:45 -0700

    initial commit

---
+++ insertdata.sql
@@ -0,0 +1,12 @@
+INSERT INTO Students(ID, Name, Age, City)
+VALUES 
+  (1, 'Manish Rathore', 21, 'Kota'),
+  (2, 'Sita Kumari', 20, 'Jaipur'),
+  (3, 'Ram Sharma', 22, 'Delhi'),
+  (4, 'Laxman Yadav', 23, 'Bikaner'),
+  (5, 'Geeta Verma', 19, 'Ajmer'),
+  (6, 'Ravi Meena', 24, 'Udaipur'),
+  (7, 'Komal Jain', 20, 'Jodhpur'),
+  (8, 'Vikas Singh', 21, 'Alwar'),
+  (9, 'Neha Saini', 22, 'Bharatpur'),
+  (10, 'Amit Joshi', 23, 'Sikar');
